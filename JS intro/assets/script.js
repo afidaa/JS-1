589a0aded89758ec4bae2d75b4f4101a39598378
Afida Valieva <afidaav@code.edu.az>
2023-03-28 01:20:18 +0400

    end

---
+++ JS intro/assets/script.js
@@ -0,0 +1,15 @@
+//first
+let number=prompt('ededi daxil edin:');
+
+if(number>0){ 
+console.log("Eded musbetdir.")
+}
+else if(number<0){
+console.log("Eded menfidir.") 
+}
+
+//second
+ let answer = prompt('ELGUN nece muellimdir?');
+ let correctAnswer = 'super';
+ let result = (answer == correctAnswer) ? 'true' : 'false';
+ alert(result);
